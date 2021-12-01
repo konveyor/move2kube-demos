624f234b42bfc8eeefcb3101134df43ef17beae1
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2021-12-01 07:37:54 +0530

    ci: make this repo part of the release workflow (#69)
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

---
+++ releasenotes-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title_prefix: "[WIP] Move2Kube Demos ",
+  // valid PR types: ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']
+  sections: [
+    { title: "🚀 Features", labels: ["enhancement", "feat", "perf"] },
+    { title: "🐛 Bug Fixes", labels: ["bug", "fix", "revert"] },
+    { title: "🧹 Maintenance", labels: ["docs", "style", "refactor", "test", "build", "ci", "chore"] },
+  ],
+  header: `For more documentation and support please visit https://move2kube.konveyor.io/
+  # Changelog`,
+  line_template: x => `- ${x.title} [#${x.number}](${x.html_url})`,
+}

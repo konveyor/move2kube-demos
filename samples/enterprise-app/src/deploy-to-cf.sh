6d405395f1f5b4f7b4d3d7fc97e62681e5d51c01
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2022-05-28 02:47:36 +0530

    fixup! fix: issues with cloud foundry deploy scripts to follow best practices
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- samples/enterprise-app/src/deploy-to-cf.sh
+++ samples/enterprise-app/src/deploy-to-cf.sh
@@ -9,119 +9,88 @@ if [[ "$(basename "$PWD")" != 'src' ]] ; then
   exit 1
 fi
 
-working_dir=$(echo $PWD)
-echo $RANDOM$RANDOM > /dev/null # In zsh, to clear the cached $RANDOM$RANDOM value
-rand1=$(echo $RANDOM$RANDOM)
-app=$(echo enterprise-app)
-appname=$(echo "$app"-$rand1)
+CF_CLI_TOOL="${CF_CLI_TOOL:-cf}"
+
+working_dir="$PWD"
+echo "$RANDOM$RANDOM" > /dev/null # In zsh, to clear the cached $RANDOM$RANDOM value
+rand1="$RANDOM$RANDOM"
+app='enterprise-app'
+appname="$app-$rand1"
 echo "appname is: $appname"
 
 trap "trap_ctrl_c" INT
 
-function trap_ctrl_c ()
-{
+function trap_ctrl_c () {
     echo "Ctrl-C caught.. Performing clean up!"
-    cd $working_dir || exit
+    cd "$working_dir" || exit
 }
 
-echo "\n======================================================================="
+echo -e "\n\n======================================================================="
 echo "========== Deploying Orders service to Cloud Foundry =================="
 echo "======================================================================="
-cd orders || exit
+cd "$working_dir"/orders || exit
 SPRING_PROFILES_ACTIVE=dev-inmemorydb ./mvnw clean package -P dev-inmemorydb
-cf push "$appname"-orders
+$CF_CLI_TOOL push "$appname"-orders
 rm -rf target
 
-echo "\n\n======================================================================="
+echo -e "\n\n======================================================================="
 echo "========== Deploying Inventory service to Cloud Foundry ==============="
 echo "======================================================================="
 cd "$working_dir"/inventory || exit
 SPRING_PROFILES_ACTIVE=dev-inmemorydb ./mvnw clean package -P dev-inmemorydb
-cf push "$appname"-inventory
+$CF_CLI_TOOL push "$appname"-inventory
 rm -rf target
 
-echo "\n\n======================================================================="
+echo -e "\n\n======================================================================="
 echo "========== Deploying Customers service to Cloud Foundry ==============="
 echo "======================================================================="
 cd "$working_dir"/customers || exit
 SPRING_PROFILES_ACTIVE=dev-inmemorydb ./mvnw clean package -P dev-inmemorydb
-cf push "$appname"-customers
+$CF_CLI_TOOL push "$appname"-customers
 rm -rf target
 
-echo "\n\n======================================================================="
+echo -e "\n\n======================================================================="
 echo "========== Deploying Gateway service to Cloud Foundry ================="
 echo "======================================================================="
 cd "$working_dir"/gateway || exit
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's/http:\/\/orders:8080/http:\/\/'"$appname"'-orders.mybluemix.net/g' src/main/resources/application-dev.properties
-    sed -i '' 's/http:\/\/customers:8080/http:\/\/'"$appname"'-customers.mybluemix.net/g' src/main/resources/application-dev.properties
-    sed -i '' 's/http:\/\/inventory:8080/http:\/\/'"$appname"'-inventory.mybluemix.net/g' src/main/resources/application-dev.properties
-else
-    sed -i 's/http:\/\/orders:8080/http:\/\/'"$appname"'-orders.mybluemix.net/g' src/main/resources/application-dev.properties
-    sed -i 's/http:\/\/customers:8080/http:\/\/'"$appname"'-customers.mybluemix.net/g' src/main/resources/application-dev.properties
-    sed -i 's/http:\/\/inventory:8080/http:\/\/'"$appname"'-inventory.mybluemix.net/g' src/main/resources/application-dev.properties
-fi
 SPRING_PROFILES_ACTIVE=dev ./mvnw clean package -P dev
-cf push "$appname"-gateway
+$CF_CLI_TOOL push "$appname"-gateway \
+    --var M2K_DEMO_CUSTOMERS_URL="http://$appname-customers.mybluemix.net/customers" \
+    --var M2K_DEMO_INVENTORY_URL="http://$appname-inventory.mybluemix.net/products" \
+    --var M2K_DEMO_ORDERS_URL="http://$appname-orders.mybluemix.net/orders"
 cd "$working_dir"/gateway || exit # If the user presses ctrl-c during previous step it will take to the $working_dir
 rm -rf target
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's/http:\/\/'"$appname"'-orders.mybluemix.net/http:\/\/orders:8080/g' src/main/resources/application-dev.properties
-    sed -i '' 's/http:\/\/'"$appname"'-customers.mybluemix.net/http:\/\/customers:8080/g' src/main/resources/application-dev.properties
-    sed -i '' 's/http:\/\/'"$appname"'-inventory.mybluemix.net/http:\/\/inventory:8080/g' src/main/resources/application-dev.properties
-else
-    sed -i 's/http:\/\/'"$appname"'-orders.mybluemix.net/http:\/\/orders:8080/g' src/main/resources/application-dev.properties
-    sed -i 's/http:\/\/'"$appname"'-customers.mybluemix.net/http:\/\/customers:8080/g' src/main/resources/application-dev.properties
-    sed -i 's/http:\/\/'"$appname"'-inventory.mybluemix.net/http:\/\/inventory:8080/g' src/main/resources/application-dev.properties
-fi
-echo "\n\n======================================================================="
+
+echo -e "\n\n======================================================================="
 echo "========== Deploying Frontend service to Cloud Foundry ================"
 echo "======================================================================="
-
 cd "$working_dir"/frontend || exit
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's|let gateway_svc.*|let gateway_svc = `http:\/\/\'"$appname"'-gateway.mybluemix.net`;|g' server.js
-    sed -i '' 's/http:\/\/localhost:8080/http:\/\/'"$appname"'-gateway.mybluemix.net/g' webpack.dev.js
-else
-    sed -i 's|let gateway_svc.*|let gateway_svc = `http:\/\/\'"$appname"'-gateway.mybluemix.net`;|g' server.js
-    sed -i 's/http:\/\/localhost:8080/http:\/\/'"$appname"'-gateway.mybluemix.net/g' webpack.dev.js
-fi
-CF_STAGING_TIMEOUT=30 cf push "$appname"-frontend
-cd "$working_dir"/frontend || exit # If the user presses ctrl-c during previous step it will take to the $working_dir
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's|let gateway_svc = `http:\/\/\'"$appname"'-gateway.mybluemix.net`;|let gateway_svc = `http:\/\/${argv.gateway}`;|g' server.js
-    sed -i '' 's/http:\/\/'"$appname"'-gateway.mybluemix.net/http:\/\/localhost:8080/g' webpack.dev.js
-else
-    sed -i 's|let gateway_svc = `http:\/\/\'"$appname"'-gateway.mybluemix.net`;|let gateway_svc = `http:\/\/${argv.gateway}`;|g' server.js
-    sed -i 's/http:\/\/'"$appname"'-gateway.mybluemix.net/http:\/\/localhost:8080/g' webpack.dev.js
-fi
-cd $working_dir || exit
+CF_STAGING_TIMEOUT=30 $CF_CLI_TOOL push "$appname"-frontend --var M2K_DEMO_GATEWAY_URL="http://$appname-gateway.mybluemix.net"
+cd "$working_dir" || exit
 
-echo "\n\n======================================================================="
+echo -e "\n\n======================================================================="
 echo "======================================================================="
-echo "\nYour appname is '$appname'"
+echo -e "\nYour appname is '$appname'"
 echo "You can use the appname to connect to the different components of the enterprise-app individually and test them."
 
-
-echo "\n\n=========== Testing the Cloud Foundry deployments of the enterprise-app services ==========="
-echo "\n$appname-inventory:"
+echo -e "\n\n=========== Testing the Cloud Foundry deployments of the enterprise-app services ==========="
+echo -e "\n$appname-inventory:"
 echo "curl http://$appname-inventory.mybluemix.net/products"
 
-echo "\n$appname-customers:"
+echo -e "\n$appname-customers:"
 echo "curl http://$appname-customers.mybluemix.net/customers"
 
-echo "\n$appname-orders:"
+echo -e "\n$appname-orders:"
 echo "curl http://$appname-orders.mybluemix.net/orders"
 
-echo "\n$appname-gateway:"
+echo -e "\n$appname-gateway:"
 echo "curl http://$appname-gateway.mybluemix.net/customers"
 echo "curl http://$appname-gateway.mybluemix.net/orders"
 echo "curl http://$appname-gateway.mybluemix.net/products"
 
-echo "\n$appname-frontend:
-Visit http://$appname-frontend.mybluemix.net URL on your browser. Please refresh the page if it appears blank."
+echo -e "\n$appname-frontend:\nVisit http://$appname-frontend.mybluemix.net URL on your browser. Please refresh the page if it appears blank."
 
-echo "\n\n======================================================================="
+echo -e "\n\n======================================================================="
 echo "======================================================================="
 
 trap - INT

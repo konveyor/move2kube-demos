13205e6fa1cec7d1d32dbd4949f851935e0c1677
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2022-05-30 16:45:23 +0530

    fixup! fixup! fixup! fixup! fix: issues with cloud foundry deploy scripts to follow best practices
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- samples/enterprise-app/src/deploy-to-ibmcloud-cf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script deploys the services of the enterprise-app to IBM Cloud Foundry.
-# Login to IBM Cloud before running this script or uncomment the command below and run the script.
-# ibmcloud login
-# ibmcloud login --sso
-# ibmcloud target -g <RESOURCE-GROUP> --cf
-# ibmcloud cf install
-
-CF_CLI_TOOL='ibmcloud cf' ./deploy-to-cf.sh
